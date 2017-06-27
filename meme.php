b551223dcec04540dd9ffd723f29bf20f3c369ef
FalFox <edoin21@gmail.com>
2017-06-27 08:33:18 +0700

    Edit

--- meme.php
+++ meme.php
@@ -44,7 +44,8 @@ function parseMemes($response){
   if($count > 0){
     $img = $data[$pick];
     while(!array_key_exists('images', $img)){
-      $img = $data[rand(1, $count)];
+      $pick = rand(1, $count);
+      $img = $data[$pick];
     }
     if(array_key_exists('images', $img)){
       return $data[$pick]['images'][0]['link'];
