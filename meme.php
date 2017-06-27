71cf8f0bddfa892a2858c1d08aad63c995e45ec9
FalFox <edoin21@gmail.com>
2017-06-27 23:59:24 +0700

    EDIT

--- meme.php
+++ meme.php
@@ -1,10 +1,47 @@
 <?php
+
 error_reporting(E_ALL);
 $memes = getImage(getMemes());
 
-print_r($memes[0] . "<br>");
 print_r($memes[1]);
 
+ ?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Meme Feeders</title>
+     <!-- Compiled and minified CSS -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/css/materialize.min.css">
+
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.99.0/js/materialize.min.js"></script>
+
+  </head>
+  <body>
+     <div class="row">
+        <div class="col s12 m4 offset-m4">
+          <div class="card">
+            <div class="card-image">
+              <img src="<?php print_r($memes[1]); ?>">
+              <span class="card-title">Card Title</span>
+            </div>
+            <div class="card-content">
+              <p>I am a very simple card. I am good at containing small bits of information.
+              I am convenient because I require little markup to use effectively.</p>
+            </div>
+            <div class="card-action">
+              <a href="#">This is a link</a>
+            </div>
+          </div>
+        </div>
+      </div>
+  </body>
+</html>
+
+<?php
 
 function getMemes(){
 
