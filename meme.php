d004bf2ce373a21b9b6555b63aceb646aacdff8f
FalFox <edoin21@gmail.com>
2017-06-27 17:28:15 +0700

    EDIT

--- meme.php
+++ meme.php
@@ -100,7 +100,7 @@ function getImage($response){
     $img = $data[$pick];
     $id = $img['cover'];
 
-    $image = json_decode(curlImage($id))['link'];
+    $image = json_decode(curlImage($id), true)['link'];
 
     $link = str_replace('http','https', $image);
     $link = explode(".", $link);
